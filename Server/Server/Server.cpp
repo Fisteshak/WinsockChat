18c449940c4e46732560bed98aea5d5e92c18110
Fisteshak <antonlazarev2005@gmail.com>
2022-06-14 16:44:40 +0300

    Инициализация сокета у сервера

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -1,20 +1,73 @@
-﻿// Server.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
 
-#include <iostream>
+#pragma comment(lib, "Ws2_32.lib")
+
+using std::cout, std::cin, std::endl;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+
+	WSADATA wsaData;
+	int errSt;   //статус ошибки
+
+
+		//инициализация интерфейса сокетов
+	errSt = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (errSt != 0) {
+		cout << "Ошибка WSAStartup " << WSAGetLastError();
+		return 1;
+	}
+	else cout << "Winsock инициализирован" << endl;
+
+		//создание и инициализация сокета
+	SOCKET servSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (servSocket == INVALID_SOCKET) {
+		cout << "Ошибка инициализации сокета " << WSAGetLastError();
+		closesocket(servSocket);
+		WSACleanup();
+		return 1;
+	}
+	else cout << "Сокет сервера создан и инициализирован" << endl;
+
+	
+	in_addr serv_ip;
+	
+
+	errSt = inet_pton(AF_INET, "127.0.0.1", &serv_ip);
+
+	if (errSt <= 0) {
+		cout << "Ошибка преобразования IP-адреса" << endl;
+		return 1;
+	}
+
+		//привзяка к сокету адреса и порта
+
+	sockaddr_in servInfo;
+	ZeroMemory(&servInfo, sizeof(servInfo));
+
+	servInfo.sin_family = AF_INET;
+	servInfo.sin_port = htons(1234);
+	servInfo.sin_addr = serv_ip;
+
+	errSt = bind(servSocket, (sockaddr*)&servInfo, sizeof(servInfo));
+	if (errSt != 0) {
+		cout << "Ошибка привязки сокета к порту и IP-адресу " << WSAGetLastError() << endl;
+		closesocket(servSocket);
+		WSACleanup();
+		return 1;
+	}
+	else cout << "Сокет успешно привязан" << endl;
+
+	
+
+
+
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
